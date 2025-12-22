087b3073a33745666ae3fd2e9733ad3b2fb19252
Maylon Lima <maylonlima923@gmail.com>
2025-12-21 23:38:20 -0300

    Adição dos exercicio solucionados referente a aula 13

--- Mundo2/ex053.py
+++ Mundo2/ex053.py
@@ -1,5 +1,15 @@
-
 """
 Desafio 53: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, 
 desconsiderando os espaços.
 """
+
+frase = input('Digite uma frase: ').replace(' ', '').lower()
+contrario = ''
+
+for letra in range(len(frase) -1, -1, -1):
+    contrario += frase[letra]
+
+if contrario == frase:
+    print('É um palíndromo')
+else:
+    print('Não é um palíndromo')
--- Mundo2/ex054.py
+++ Mundo2/ex054.py
@@ -1,5 +1,16 @@
-
 """
 Desafio 54: Crie um programa que leia o ano de nascimento de sete pessoas. 
 No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
 """
+
+from datetime import date
+
+cont = 0
+
+for i in range(0, 7):
+    nasc = int(input('Digite a data de nascimento: '))
+    idade = date.today().year - nasc
+    if idade < 18:
+        cont += 1
+
+print(f'Temos {cont} pessoas menores de idade e {7-cont} pessoas maiores de idade')
